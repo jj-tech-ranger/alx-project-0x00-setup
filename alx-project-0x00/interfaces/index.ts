a46e235ea0f2fe7c10f86b5a564f84bd00eba679
Jemimah Jemutai <jemutaijemimah@gmail.com>
2025-11-12 12:04:08 +0300

    Create index.ts

---
+++ alx-project-0x00/interfaces/index.ts
@@ -0,0 +1,15 @@
+export interface PillProps {
+  title: string;
+}
+
+export interface ButtonProps {
+  title: string;
+  styles?: string;
+}
+
+export interface CardProps {
+  image?: string;
+  title?: string;
+  location?: string;
+  rating?: number;
+}
