3207f72b7b7eda27a7a788be3a177f80b3f9c79b
Jemimah Jemutai <jemutaijemimah@gmail.com>
2025-11-12 12:52:06 +0300

    Update landing.tsx

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,10 +1,14 @@
 pages/landing.tsx  import Card from "@/components/Card";
+import Button from "@/components/Button";
 
 const Landing: React.FC =  () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
+            <Button title="Small Button" styles="rounded-sm" />
+      <Button title="Large Button" styles="rounded-lg" />
+      <Button title="Full Button" styles="rounded-full" />
     </div>
   )
 }
